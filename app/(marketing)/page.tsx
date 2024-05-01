11945ea2ac7ea10e78e0aaa11b20806cacdc04bd
prabath1998 <prabhathudayanga1998@gmail.com>
2024-05-01 20:53:19 +0530

    changes on landing pade

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,19 +1,33 @@
-import { Medal } from 'lucide-react'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Medal } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const MarketingPage = () => {
   return (
-    <div className='flex items-center justify-center flex-col'>
-        <div className='flex items-center justify-center flex-col'>
-            <div className='mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase'>
-                <Medal className='h-6 w-6 mr-2'/>
-                No 1 Task Management
-            </div>
-            <h1 className='text-3xl md:text-6xl text-center text-neutral-800'>Ontrack helps team move</h1>
+    <div className="flex items-center justify-center flex-col">
+      <div className="flex items-center justify-center flex-col">
+        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+          <Medal className="h-6 w-6 mr-2" />
+          No 1 Task Management
         </div>
-     
+        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
+          Ontrack helps team move
+        </h1>
+        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+          Work forward
+        </div>
+      </div>
+      <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+        Collaborate, manage projects, and reach new productivity peaks. From
+        high rises to the home office, the way your team works is unique -
+        accomplish it all with Ontrack.
+      </div>
+      <Button className="mt-6" size="lg" asChild>
+        <Link href="sign-up">Get Ontrack for free</Link>
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default MarketingPage
+export default MarketingPage;
