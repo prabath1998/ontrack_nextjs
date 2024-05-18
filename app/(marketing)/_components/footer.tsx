a76a396e8bedf9fd2e9d1bcd43f09f02be5073d4
prabath1998 <prabhathudayanga1998@gmail.com>
2024-05-18 23:02:22 +0530

    add footer

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
+
+
+const Footer = () => {
+  return (
+    <div className="fixed bottom-0 w-full p-4 border-t bg-slate-100">
+      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <Logo />
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+          <Button variant="ghost" size="sm">Privacy policy</Button>
+          <Button size="sm" variant="ghost">Terms of service</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
